3d31ad3f4b7f3d24f2640d34fde1f567e87264ec
duongwolf2510 <84836850+duongwolf2510@users.noreply.github.com>
2021-06-22 02:09:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Boo à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introDesc: `Hôm nay là kỉ niệm 3 tháng hai đứa mình yêu nhau
+    Nên là anh có làm cái Web nhỏ nhắn này để dành tặng riêng em
+    Chúc cho hai đứa mình tiếp tục yêu nhau hơn cả trước và luôn cố gắng vì nhau nhé ☺☺
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Uầyyy',
+    btnIntro: 'Happy 3 months anniversary 🎉🎉',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Em ghét anh nhiều lắm <33',
-    btnNo: 'Em yêu anh rất nhiều :3',
+    btnYes: 'Em ghét anh nhiều lắm 😂',
+    btnNo: 'Em yêu anh rất nhiều 😜',
     question:'Trên thế giới hơn 7 tỉ người nhưng mà đối với anh nụ cười của em vẫn là đẹp nhất ❤. Em có biết tại sao không? 😚',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Vì em là cô người yêu đáng yêu xinh đẹp tuyệt vời của anh <33333333',
+    reply: 'Tại vì em là cô người yêu đáng yêu xinh đẹp tuyệt vời của anh á ❤💘💖💝',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Xíu nữa anh gọi cho em nhé công chúa nhỏ của anh <3',
     btnAccept: 'Okiiiii lun ❤',
