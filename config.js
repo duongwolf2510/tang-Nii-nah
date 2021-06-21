a8003101be9871aa5eb4fe474b4250c1e8fb002e
duongwolf2510 <84836850+duongwolf2510@users.noreply.github.com>
2021-06-22 01:59:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Uầyyy',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Em ghét anh nhiều lắm <33',
     btnNo: 'Em yêu anh rất nhiều :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người nhưng mà đối với anh nụ cười của em vẫn là đẹp nhất ❤. Em có biết tại sao không? 😚',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì em là cô người yêu đáng yêu xinh đẹp tuyệt vời của anh <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Xíu nữa anh gọi cho em nhé công chúa nhỏ của anh <3',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun ❤',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
