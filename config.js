5314e419816b149497d3b13585becf6d1da93efd
duongwolf2510 <84836850+duongwolf2510@users.noreply.github.com>
2021-06-25 17:09:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Boo à!',
-    introDesc: `Hôm nay là kỉ niệm 3 tháng của chúng mình
-    Nên anh đã làm món quà nhỏ để dành tặng riêng em
-    Mong hai đứa mình sẽ tiếp tục yêu nhau nhiều hơn 😊
-    Rất vui vì 3 tháng trước đã làm quen với em 😅`,
+    introDesc: `Hôm nay là kỉ niệm 3 tháng của chúng mình rồi á
+    Nên anh đã làm món quà nhỏ để dành tặng em
+    Món quà tuy không phải là tuyệt nhất 😅
+    Nhưng mà nó là tất cả tình cảm của anh dành cho em 😊`,
     btnIntro: 'Happy 3 months anniversary 🎉🎉',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Em làm tâm trí anh lu mờ, nhưng mà là mờ ê MÊ 😙',
+    desc: 'Nhưng mà anh hỏi thật nhé, 3 tháng qua em có cảm xúc như nào với anh?',
     btnYes: 'Em ghét anh nhiều lắm 😂',
     btnNo: 'Em yêu anh rất nhiều 😜',
     question:'Trên thế giới hơn 7 tỉ người nhưng mà đối với anh nụ cười của em vẫn là đẹp nhất ❤. Em có biết tại sao không? 😚',
     btnReply: 'Gửi cho anh <3',
     reply: 'Tại vì em là cô người yêu đáng yêu xinh đẹp tuyệt vời của anh á ❤❤❤❤',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Xíu nữa anh gọi cho em nhé công chúa nhỏ của anh <3',
-    btnAccept: 'Okiiiii lun ❤',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Nhưng mà em phải nhìn mặt em lúc này cơ. Trông đáng yêu lắm ❤',
+    btnAccept: 'Ghét anh lắm ấy ❤',
+    //messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
