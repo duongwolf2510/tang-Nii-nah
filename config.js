462000855c4b6253958a6aa955cb36e13bcc6434
duongwolf2510 <84836850+duongwolf2510@users.noreply.github.com>
2021-07-08 22:43:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ const CONFIG = {
     introDesc: `Hôm nay là kỉ niệm 3 tháng của chúng mình rồi á
     Nên anh đã làm món quà nhỏ để dành tặng em
     Món quà tuy không phải là tuyệt nhất 😅      
-    Nhưng mà nó là tất cả tình cảm của anh dành cho em 😊`,
+    Nhưng mà nó là tình cảm của anh dành cho em 😊`,
     btnIntro: 'Happy 3 months anniversary 🎉🎉',
-    title: 'Em làm tâm trí anh lu mờ, nhưng mà là mờ ê MÊ 😙',
+    title: 'Có nhạc bên cạnh ai không phiêu Có em bên cạnh yêu không phai 😙',
     desc: 'Nhưng mà anh hỏi thật nhé, 3 tháng qua em có cảm xúc như nào với anh?',
     btnYes: 'Em ghét anh nhiều lắm 😂',
     btnNo: 'Em yêu anh rất nhiều 😜',
-    question:'Trên thế giới hơn 7 tỉ người nhưng mà đối với anh nụ cười của em vẫn là đẹp nhất ❤. Em có biết tại sao không? 😚',
+    question:'Ơ kìa =)))) Nhưng em biết không, trên thế giới hơn 7 tỉ người nhưng mà đối với anh nụ cười của em vẫn là đẹp nhất ❤. Em có biết tại sao không? 😚',
     btnReply: 'Gửi cho anh <3',
     reply: 'Tại vì em là cô người yêu đáng yêu xinh đẹp tuyệt vời của anh á ❤❤❤❤',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
