3d940c66752df43adb6c4f300968c3766a8cdf8a
duongwolf2510 <84836850+duongwolf2510@users.noreply.github.com>
2021-07-08 22:47:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const CONFIG = {
     introTitle: 'Boo à!',
     introDesc: `Hôm nay là kỉ niệm 3 tháng của chúng mình rồi á
     Nên anh đã làm món quà nhỏ để dành tặng em
-    Món quà tuy không phải là tuyệt nhất 😅      
+    Món quà tuy không phải là tuyệt nhất 😅        
     Nhưng mà nó là tình cảm của anh dành cho em 😊`,
     btnIntro: 'Happy 3 months anniversary 🎉🎉',
-    title: 'Có nhạc bên cạnh ai không phiêu 🤗 Có em bên cạnh yêu không phai 😙',
+    title: 'Có nhạc bên cạnh ai không phiêu 🤗    Có em bên cạnh yêu không phai 😙',
     desc: 'Nhưng mà anh hỏi thật nhé, 3 tháng qua em có cảm xúc như nào với anh?',
     btnYes: 'Em ghét anh nhiều lắm 😂',
     btnNo: 'Em yêu anh rất nhiều 😜',
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Tại vì em là cô người yêu đáng yêu xinh đẹp tuyệt vời của anh á ❤❤❤❤',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Nhưng mà em phải nhìn mặt em lúc này cơ. Trông đáng yêu lắm ❤',
+    messDesc: 'Nhưng mà em phải nhìn mặt em lúc này cơ, trông đáng yêu lắm ❤ Giờ em tham khảo cái này nhé ❤',
     btnAccept: 'Ghét anh lắm ấy ❤',
     messLink: 'https://www.dichvucong.gov.vn/p/home/dvc-chi-tiet-thu-tuc-hanh-chinh.html?ma_thu_tuc=1.000894' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
